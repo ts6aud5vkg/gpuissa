a1f4e0e1b4383764598c4a95e526c315d411d385
ts6aud5vkg <44915776+ts6aud5vkg@users.noreply.github.com>
2019-11-11 18:46:04 +0700

    Update cpu_deamon.sh

--- cpu_deamon.sh
+++ cpu_deamon.sh
@@ -4,7 +4,7 @@
 # ==============================================================
 
 # Variables
-CPU_LIMIT=300
+CPU_LIMIT=350
 DAEMON_INTERVAL=3  	
 BLACK_PROCESSES_LIST=   
 WHITE_PROCESSES_LIST=  
